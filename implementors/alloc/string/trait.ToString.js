863bb05f9b69d7ab40bf62ece1a196eb6cef520d
Travis CI User <travis@example.org>
2017-09-10 19:52:33 +0000

    Documentation for rust-syndication/rss@0b88fb3

---
+++ implementors/alloc/string/trait.ToString.js
@@ -0,0 +1,13 @@
+(function() {var implementors = {};
+implementors["error_chain"] = [];
+implementors["libc"] = [];
+implementors["rss"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"rss/struct.Channel.html\" title=\"struct rss::Channel\">Channel</a>",];
+implementors["syn"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/std/net/addr/trait.ToSocketAddrs.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["error_chain"] = [];
+implementors["libc"] = [];
+implementors["syn"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
